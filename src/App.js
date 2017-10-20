d7d857e32c3b34c862e8913f48ff533dc74e4378
Francisco Arriagada <jfarriagada91@gmail.com>
2017-10-20 00:48:29 -0300

    add firebase redux base

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// redux 
+import { connect } from 'react-redux'
+// Firebase
+import { DB_CONFIG } from './firebase'
+import firebase from 'firebase'
+
+// Firebase config
+firebase.initializeApp(DB_CONFIG);
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      return (
+        <div>
+          Aloja :D
+        </div>
+      )
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+      
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
---
+++ src/Redux/store.js
@@ -0,0 +1,13 @@
+import { createStore, combineReducers } from 'redux'
+// redux form
+import { reducer as formReducer } from 'redux-form'
+
+// reducers que devuelven un nuevo estado
+
+const reducer = combineReducers({
+
+})
+
+const store = createStore(reducer)
+
+export default store
