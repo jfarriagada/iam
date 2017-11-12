b87263b1f6e8cb7f3d706518e4b4d3e872dbeb67
Francisco Arriagada <jfarriagada91@gmail.com>
2017-11-12 20:21:19 -0300

    css post image

--- src/Components/UI/Post.js
+++ src/Components/UI/Post.js
@@ -4,8 +4,13 @@ import { Link } from 'react-router-dom'
 const Post = (props) => {
     return (
         <div>
-            <img src={props.banner_url} height="200" />
-            <h1 className="title"><Link to={`/${props.id}`}>{props.title}</Link></h1>
+            <figure>
+                <img src={props.banner_url} />
+                <figcaption>
+                    <p className="titles"><Link to={`/${props.id}`}>{props.title}</Link></p>
+                </figcaption>
+            </figure>
+            <hr />
             <p>{props.body}</p>
             <strong className="column is-offset-9">{props.day} / {props.month} / {props.year}</strong>
             <br/>
